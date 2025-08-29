e9dff925cd02ebfec80cceb16b1b15c16a5d77fd
xwaspy <skytechx3@gmail.com>
2025-08-29 15:11:25 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -43,30 +43,36 @@ const Home = () => {
           </p>
         </div>
         <div className="flex gap-3">
-          <BiLogoGithub
-            className="h-10 w-10 cursor-pointer rounded-full 
-        border-2 border-transparent bg-purple-500 text-white
-        transition-all duration-200 hover:scale-110
-         hover:border-purple-600 hover:bg-white
-         hover:text-purple-600 hover:translate-y-3 md:h-12 md:w-12"
-          />
-
-          <BiLogoLinkedin
-            className="h-10 w-10 cursor-pointer rounded-full 
-        border-2 border-transparent bg-purple-500 text-white
-        transition-all duration-200 hover:scale-110
-         hover:border-purple-600 hover:bg-white
-         hover:text-purple-600 hover:translate-y-3 md:h-12 md:w-12"
-          />
-
-          <BiLogoInstagram
-            className="h-10 w-10 cursor-pointer rounded-full 
-        border-2 border-transparent bg-purple-500 text-white
-        transition-all duration-200 hover:scale-110
-         hover:border-purple-600 hover:bg-white
-         hover:text-purple-600 hover:translate-y-3 md:h-12 md:w-12"
-          />
+          <a
+            href="https://github.com/xwaspy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BiLogoGithub
+              className="h-10 w-10 cursor-pointer rounded-full 
+      border-2 border-transparent bg-purple-500 text-white
+      transition-all duration-200 hover:scale-110
+      hover:border-purple-600 hover:bg-white
+      hover:text-purple-600 hover:translate-y-3 md:h-12 md:w-12"
+            />
+          </a>
         </div>
+
+        <BiLogoLinkedin
+          className="h-10 w-10 cursor-pointer rounded-full 
+        border-2 border-transparent bg-purple-500 text-white
+        transition-all duration-200 hover:scale-110
+         hover:border-purple-600 hover:bg-white
+         hover:text-purple-600 hover:translate-y-3 md:h-12 md:w-12"
+        />
+
+        <BiLogoInstagram
+          className="h-10 w-10 cursor-pointer rounded-full 
+        border-2 border-transparent bg-purple-500 text-white
+        transition-all duration-200 hover:scale-110
+         hover:border-purple-600 hover:bg-white
+         hover:text-purple-600 hover:translate-y-3 md:h-12 md:w-12"
+        />
       </div>
     </div>
   );
